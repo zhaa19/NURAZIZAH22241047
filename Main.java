ef769447ab440d358299fc79c7770b3bc156f0c9
zhaa19 <146044576+zhaa19@users.noreply.github.com>
2023-11-28 10:37:57 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,74 @@
 package com.tutorial;
 
-class Polos{
-   String dataString;
-   int dataInterger;
+class Mahasiswa {
+
+// Data Member
+String nama;
+String nim;
+String prodi;
+
+//konstruktor
+Mahasiswa( String nama, String nim, String prodi){
+    this.nama = nama;
+    this.nim = nim;
+    this.prodi = prodi;
 }
 
+// method tanpa return dan tanpa parameter
+void show(){
+    System.out.println("Nama : " + this.nama);
+    System.out.println("NIM : " + this.nim);
+    System.out.println("Prodi : " + this.prodi);
+
+
+}
+
+// method tanpa dengan parameter
+void setNama (String nama, String nim, String prodi) {
+    this.nama = nama;
+    this.nim = nim;
+    this.prodi = prodi; 
+}
+
+
+// method tanpa parameter dengan return
+String getNama(){
+    return this.nama;
+
+}                                          
+ String getNIM(){
+    return this.nim;
+ }
+
+ // method dengan parameter dengan return
+ String sayHi(String pesan) {
+    return pesan + " aku " + this.nama + " Mahasiswa PTI ";
+ }
+
+
+
+
+
+
+}
 public class Main {
-   public static void main(String[] args) {
-      // intansiasi objek
-      Polos pls = new Polos();
-
-      // memberi nilai pada objek pls
-      pls.dataString = "polos";
-      pls.dataInterger = 110;
-
-      System.out.println("dataString : " + pls.dataString);
-      System.out.println("dataInterger : " + pls.dataInterger);
 
 
-   }
-}
+    public static void main(String[] args) {
+        
+        // membuat objek baru atau instansiasi
+        Mahasiswa mhs1 = new Mahasiswa ("Azizah","12345678", "PTI");
+        
+        // memanggil method
+        mhs1.show();
+        mhs1.setNama("indraa", "22241047", "b inggris");
+        mhs1.show();
+
+        System.out.println(mhs1.getNama());
+        System.out.println(mhs1.getNIM());
+
+        String pesan = mhs1.sayHi("haii");
+        System.out.println(pesan);
+    }
+
+} 
